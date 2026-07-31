648bafbab08fd55ea2c1bfc42854b096b517297e
Luis Contreras <ldc876@gmail.com>
2026-07-30 22:08:06 -0500

    random emoijis

---
+++ 021. Random Emojis/app.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('#emoji')
+
+const emojis = ["😀", "🚀", "🍕", "🐶", "🔥", "🎮", "🌈", "💻", "⚽", "👻"];
+
+btn.addEventListener('mouseover', () => {
+  btn.innerHTML = emojis[Math.floor(Math.random()* emojis.length)]
+})
