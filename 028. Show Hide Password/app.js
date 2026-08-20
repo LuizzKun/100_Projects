543c7b27f34b932faa9e964dc3913c68a089cd4d
Luis Contreras <ldc876@gmail.com>
2026-08-20 18:08:18 -0500

    Hide & Show password

---
+++ 028. Show Hide Password/app.js
@@ -0,0 +1,17 @@
+// input
+//eye
+
+const input = document.querySelector('input')
+const eye= document.querySelector('#eye-icon')
+
+eye.addEventListener('click', () => {
+  // if(input.type === 'password') {
+  //   input.type = 'text'
+  // } else {
+  //   input.type = 'password'
+  // }
+
+  //Refactor
+
+  input.type === 'password' ? (input.type = 'text') : (input.type = 'password')
+})
