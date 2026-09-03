dc3133ee7331b9ec41c06f2f7ea28120a789fcd8
Luizz <ldc876@gmail.com>
2026-09-03 00:52:04 -0500

    Counter

---
+++ 033.Counter/app.js
@@ -0,0 +1,25 @@
+let countEl  = document.getElementById('count')
+let saveEl = document.getElementById('save')
+let incrementBtn =  document.querySelector('.increment-btn')
+let saveBtn  = document.querySelector(".save-btn")
+
+console.log(countEl);
+console.log(saveEl);
+console.log(incrementBtn);
+console.log(saveBtn);
+
+
+let count =  0;
+
+
+incrementBtn.addEventListener('click', () =>  {
+  count += 1
+  countEl.textContent =  count;
+});
+
+saveBtn.addEventListener('click', () => {
+  let countStr = count  +  ",  "
+  saveEl.textContent += countStr
+  countEl.textContent =  0
+  count = 0;
+})
